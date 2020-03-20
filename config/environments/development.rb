5498550f8e55e1acdd8fad6d7dc555062009b2e1
Mike Hacker <desertsofcacti@gmail.com>
2020-03-20 16:16:30 -0500

    hostnames

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,4 +49,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Whitelist one hostname
+  config.hosts << "awesome-rails-api.test.com"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # Whitelist Heroku hostname
+  config.hosts << awesome-rails-api.herokuapp.com
 end
